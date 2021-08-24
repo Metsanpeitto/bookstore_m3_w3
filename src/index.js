3e01daf6d96e418495602370373c5d330087f2e3
Abraham R <abraham@waldenberginc.com>
2021-08-24 23:22:25 +0200

    Add changes requested by the cod ereviewer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
-import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
